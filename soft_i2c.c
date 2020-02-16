1da8195561e1983d10b0779e5137de47a2255997
Pengzhen Zhao <hackercircle@163.com>
2020-02-16 17:25:24 +0800

    soft i2c on stm32 ll lib
    soft i2c on stm32 ll lib

---
+++ soft_i2c.c
@@ -0,0 +1,247 @@
+/***
+	Copyright (c) 2020
+	All rights reserved.
+
+	File name           :soft_i2c.c
+	file identification :
+	Subject             :Say something about the file
+
+	Current Version     :V1.0.0
+	Author              :Hacker
+	Date                :
+
+	Instead Version     :
+	Author              :Hacker
+	Date                :
+***/
+#include "soft_i2c.h"
+
+
+//产生I2C起始信号
+void I2C_Start(void)
+{
+	SET_SDA_OUT();     //sda线输出
+	I2C_SDA_1();	  	  
+	I2C_SCL_1();
+	NOP(4);
+ 	I2C_SDA_0();//START:when CLK is high,DATA change form high to low 
+	NOP(4);
+	I2C_SCL_0();//钳住I2C总线，准备发送或接收数据 
+}
+
+//产生I2C停止信号
+void I2C_Stop(void)
+{
+	SET_SDA_OUT();//sda线输出
+	I2C_SCL_0();
+	I2C_SDA_0();//STOP:when CLK is high DATA change form low to high
+ 	NOP(4);
+	I2C_SCL_1(); 
+	I2C_SDA_1();//发送I2C总线结束信号
+	NOP(4);							   	
+}
+//等待应答信号到来
+//返回值：1，接收应答失败
+//        0，接收应答成功
+uint8_t I2C_Wait_Ack(void)
+{
+	uint16_t ucErrTime = 0;
+	SET_SDA_IN();      //SDA设置为输入  
+	I2C_SDA_1();NOP(1);	   
+	I2C_SCL_1();NOP(1);	 
+	while(I2C_SDA_READ())
+	{
+		ucErrTime ++;
+		if(ucErrTime > 250)
+		{
+			I2C_Stop();
+			return 1;
+		}
+	}
+	I2C_SCL_0();//时钟输出0 	   
+	return 0;  
+}
+
+//产生ACK应答
+void I2C_Ack(void)
+{
+	I2C_SCL_0();
+	SET_SDA_OUT();
+	I2C_SDA_0();
+	NOP(2);
+	I2C_SCL_1();
+	NOP(2);
+	I2C_SCL_0();
+}
+
+//不产生ACK应答		    
+void I2C_NAck(void)
+{
+	I2C_SCL_0();
+	SET_SDA_OUT();
+	I2C_SDA_1();
+	NOP(2);
+	I2C_SCL_1();
+	NOP(2);
+	I2C_SCL_0();
+}
+
+//I2C发送一个字节
+//返回从机有无应答
+//1，有应答
+//0，无应答			  
+void I2C_Send_Byte(uint8_t txd)
+{                        
+    uint8_t t;   
+	SET_SDA_OUT(); 	    
+    I2C_SCL_0();//拉低时钟开始数据传输
+    for(t = 0;t < 8;t ++)
+    {              
+		if((txd&0x80) >> 7)
+			I2C_SDA_1();
+		else
+			I2C_SDA_0();
+		txd <<= 1; 	  
+		NOP(2);   
+		I2C_SCL_1();
+		NOP(2); 
+		I2C_SCL_0();	
+		NOP(2);
+    }	 
+}
+
+
+//读1个字节，ack=1时，发送ACK，ack=0，发送nACK   
+uint8_t I2C_Rece_Byte(unsigned char ack)
+{
+	unsigned char i,receive = 0;
+	SET_SDA_IN();//SDA设置为输入
+    for(i = 0;i < 8;i ++)
+	{
+        I2C_SCL_0(); 
+        NOP(2);
+		I2C_SCL_1();
+        receive <<= 1;
+        if(I2C_SDA_READ())
+        	receive++;   
+		NOP(1); 
+    }					 
+    if (!ack)
+        I2C_NAck();//发送nACK
+    else
+        I2C_Ack(); //发送ACK
+
+    SET_SDA_OUT(); 	    
+	I2C_SDA_1();
+   
+    return receive;
+}
+
+/***
+	send one frame by iic
+***/
+void i2cAction(uint8_t deviceAddr, uint8_t regAddress)
+{
+	I2C_Start();
+	I2C_Send_Byte(deviceAddr<<1);
+	I2C_Wait_Ack();
+	I2C_Send_Byte(regAddress);
+	I2C_Wait_Ack();
+	I2C_Stop();
+}
+
+uint8_t  I2C_Write_Byte(uint8_t deviceAddr, uint8_t regAddress, uint8_t content)
+{
+	uint8_t err = 0;
+	I2C_Start();
+	I2C_Send_Byte(deviceAddr<<1);
+    err = I2C_Wait_Ack();
+
+	I2C_Send_Byte(regAddress);
+    err = I2C_Wait_Ack();
+
+	I2C_Send_Byte(content);
+	err = I2C_Wait_Ack();
+	I2C_Stop();	
+	return err;
+}
+
+uint8_t  I2C_Read_Byte(uint8_t deviceAddr, uint8_t regAddress, uint8_t *content)
+{
+	uint8_t err = 0;
+    I2C_Start();
+    I2C_Send_Byte(deviceAddr<<1);
+    err = I2C_Wait_Ack();
+
+    I2C_Send_Byte(regAddress);
+    err = I2C_Wait_Ack();
+
+    I2C_Start();
+    I2C_Send_Byte((deviceAddr<<1)|0x01);
+    err = I2C_Wait_Ack();
+
+    *content = I2C_Rece_Byte(0);//data read error
+    I2C_Stop();
+    return err;
+}
+
+/**
+ * @brief [write blocks]
+ * @details [long description]
+ * 
+ * @param deviceAddr [description]
+ * @param regAddress [description]
+ * @param content [description]
+ * @param size [num of bytes]
+ * @return [0->ok,1->error]
+ */
+uint8_t  I2C_Write_Bytes(uint8_t deviceAddr, uint8_t regAddress, const uint8_t *content, uint16_t size)
+{
+	uint8_t err = 0;
+	I2C_Start();
+	I2C_Send_Byte(deviceAddr<<1);
+    err = I2C_Wait_Ack();
+
+	I2C_Send_Byte(regAddress);
+    err = I2C_Wait_Ack();
+
+    for (int i = 0; i < size; ++i)
+    {
+		I2C_Send_Byte(*(content+i));
+		err = I2C_Wait_Ack();	
+    }
+
+	I2C_Stop();	
+	return err;
+}
+
+/**
+ * @brief read blocks 
+ * @details [long description]
+ * 
+ * @param deviceAddr [description]
+ * @param regAddress [description]
+ * @param content [description]
+ * @param size [num of bytes]
+ * @return [0->ok,1->error]
+ */
+uint8_t  I2C_Read_Bytes(uint8_t deviceAddr, uint8_t regAddress, uint8_t *content, uint16_t size)
+{
+	uint8_t err = 0;
+    I2C_Start();
+    I2C_Send_Byte(deviceAddr<<1);
+    err = I2C_Wait_Ack();
+
+    I2C_Send_Byte((regAddress<<1) | 0x01);
+
+    for (int i = 0; i < size; ++i)
+    {
+	    err = I2C_Wait_Ack();
+    	*(content + i) = I2C_Rece_Byte(1);//data read error
+    }
+
+    I2C_Stop();
+    return err;
+}
+
+
---
+++ soft_i2c.h
@@ -0,0 +1,100 @@
+/***
+    Copyright (c) 2020
+    All rights reserved.
+
+    File name           :soft_i2c.h
+    file identification :
+    Subject             :`
+
+    Current Version     :V1.0.0
+    Author              :Hacker
+    Date                :
+
+    Instead Version     :
+    Author              :Hacker
+    Date                :
+***/
+
+#ifndef __SOFT_I2C__
+#define __SOFT_I2C__
+
+#include "main.h"
+ 
+#define GPIO_PORT_I2C   GPIOA                // GPIO端口 
+#define I2C_SCL_PIN     SCL_Pin          // 连接到SCL时钟线的GPIO 
+#define I2C_SDA_PIN     SDA_Pin          // 连接到SDA数据线的GPIO 
+ 
+#define SET_SDA_IN()    LL_GPIO_SetPinMode(GPIO_PORT_I2C, I2C_SDA_PIN, LL_GPIO_MODE_INPUT)
+#define SET_SDA_OUT()   LL_GPIO_SetPinMode(GPIO_PORT_I2C, I2C_SDA_PIN, LL_GPIO_MODE_OUTPUT)
+ 
+/* 定义读写SCL和SDA的宏 */
+// #define I2C_SCL_1()  GPIO_PORT_I2C->BSRR = I2C_SCL_PIN                                          // SCL = 1 
+// #define I2C_SCL_0()  GPIO_PORT_I2C->BSRR = (uint32_t)I2C_SCL_PIN << 16U         // SCL = 0 
+ 
+// #define I2C_SDA_1()  GPIO_PORT_I2C->BSRR = I2C_SDA_PIN                                          // SDA = 1 
+// #define I2C_SDA_0()  GPIO_PORT_I2C->BSRR = (uint32_t)I2C_SDA_PIN << 16U         // SDA = 0 
+
+// #define I2C_SDA_READ()  (GPIO_PORT_I2C->IDR & I2C_SDA_PIN)                     // 读SDA口线状态 
+// #define I2C_SCL_READ()  (GPIO_PORT_I2C->IDR & I2C_SCL_PIN)                    // 读SCL口线状态 
+
+
+#define I2C_SCL_1()  LL_GPIO_SetOutputPin(GPIO_PORT_I2C,   I2C_SCL_PIN)                                         // SCL = 1 
+#define I2C_SCL_0()  LL_GPIO_ResetOutputPin(GPIO_PORT_I2C, I2C_SCL_PIN)         // SCL = 0 
+#define I2C_SDA_1()  LL_GPIO_SetOutputPin(GPIO_PORT_I2C,   I2C_SDA_PIN)                         // SDA = 1 
+#define I2C_SDA_0()  LL_GPIO_ResetOutputPin(GPIO_PORT_I2C, I2C_SDA_PIN)         // SDA = 0 
+
+#define I2C_SDA_READ()  (LL_GPIO_ReadInputPort(GPIO_PORT_I2C) & I2C_SDA_PIN)     // 读SDA口线状态 
+#define I2C_SCL_READ()  (LL_GPIO_ReadInputPort(GPIO_PORT_I2C) & I2C_SCL_PIN)      // 读SCL口线状态 
+
+ 
+#define NOP1()  __nop()
+#define NOP2()  NOP1(),NOP1()
+#define NOP3()  NOP2(),NOP1()
+#define NOP4()  NOP3(),NOP1()
+#define NOP5()  NOP4(),NOP1()
+#define NOP6()  NOP5(),NOP1()
+#define NOP7()  NOP6(),NOP1()
+#define NOP8()  NOP7(),NOP1()
+#define NOP9()  NOP8(),NOP1()
+#define NOP10() NOP9(),NOP1()
+#define NOP11() NOP10(),NOP1()
+#define NOP12() NOP11(),NOP1()
+#define NOP13() NOP12(),NOP1()
+#define NOP14() NOP13(),NOP1()
+#define NOP15() NOP14(),NOP1()
+#define NOP16() NOP15(),NOP1()
+#define NOP17() NOP16(),NOP1()
+#define NOP18() NOP17(),NOP1()
+#define NOP19() NOP18(),NOP1()
+#define NOP20() NOP19(),NOP1()
+#define NOP21() NOP20(),NOP1()
+#define NOP22() NOP21(),NOP1()
+#define NOP23() NOP22(),NOP1()
+#define NOP24() NOP23(),NOP1()
+#define NOP25() NOP24(),NOP1()
+#define NOP26() NOP25(),NOP1()
+#define NOP27() NOP26(),NOP1()
+#define NOP28() NOP27(),NOP1()
+#define NOP29() NOP28(),NOP1()
+#define NOP30() NOP29(),NOP1()
+#define NOP31() NOP30(),NOP1()
+#define NOP32() NOP31(),NOP1()
+#define NOP33() NOP32(),NOP1()
+#define NOP34() NOP33(),NOP1()
+#define NOP35() NOP34(),NOP1()
+#define NOP36() NOP35(),NOP1()
+#define NOP37() NOP36(),NOP1()
+#define NOP38() NOP37(),NOP1()
+#define NOP39() NOP38(),NOP1()
+#define NOP40() NOP39(),NOP1()
+#define NOP(N)  NOP##N##0()
+
+//I2C所有操作函数
+               
+
+uint8_t  I2C_Write_Byte(uint8_t deviceAddr, uint8_t regAddress, uint8_t content);
+uint8_t  I2C_Read_Byte(uint8_t deviceAddr, uint8_t regAddress, uint8_t *content);
+uint8_t  I2C_Write_Bytes(uint8_t deviceAddr, uint8_t regAddress, const uint8_t *content, uint16_t size);
+uint8_t  I2C_Read_Bytes(uint8_t deviceAddr, uint8_t regAddress, uint8_t *content, uint16_t size);
+
+#endif
